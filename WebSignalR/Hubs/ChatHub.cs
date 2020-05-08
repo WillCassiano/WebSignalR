cc32c00bd6a3a3e26b97c21e6ba0b16f506e1c35
Willian Braga Cassiano <llchivell@gmail.com>
2020-05-08 12:03:21 -0400

    Projeto Inicial

---
+++ WebSignalR/Hubs/ChatHub.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Threading.Tasks;
+
+namespace WebSignalR.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public static IDictionary<string, string> hashUser = new Dictionary<string, string>();
+        public async Task EnviarMensagem(string usuario, string mensagem)
+        {
+            var teste = hashUser.Keys.FirstOrDefault(p => p == this.Context.ConnectionId);
+            if (teste == null)
+                await AddNewUser(usuario);
+
+            await Clients.All.SendAsync("ReceberMensagem", usuario, mensagem);
+        }
+
+        public async Task AddNewUser(string name)
+        {
+            hashUser.Add(this.Context.ConnectionId, name);
+            await Clients.All.SendAsync("NewUserAdd", name);
+        }
+    }
+}
---
+++ WebSignalR/wwwroot/js/chat.js
@@ -0,0 +1,37 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+var enviarButton = document.getElementById("enviarButton");
+var usuarioInput = document.getElementById("usuarioInput");
+var mensagemInput = document.getElementById("mensagemInput");
+
+enviarButton.disabled = true;
+
+connection.on("ReceberMensagem", function (usuario, mensagem) {
+    var msg = mensagem.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var encodedMsg = usuario + " disse " + msg;
+    var li = document.createElement("li");
+    li.textContent = encodedMsg;
+    document.getElementById("mensagemLista").appendChild(li);
+});
+
+connection.on("NewUserAdd", function (name) {
+    var li = document.createElement("li");
+    li.textContent = name;
+    document.getElementById("usuarioLista").appendChild(li);
+});
+
+connection.start().then(function () {
+    enviarButton.disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+enviarButton.addEventListener('click', function (event) {
+    var usuario = usuarioInput.value;
+    var mensagem = mensagemInput.value;
+    connection.invoke("EnviarMensagem", usuario, mensagem).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
